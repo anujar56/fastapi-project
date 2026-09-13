5ae232e724225b30941fa06e5419a1c6e008af8b
laborant <laborant@labs.iximiuz.com>
2026-09-13 13:22:04 +0000

    adding jinja variables for dynamic content

--- main.py
+++ main.py
@@ -27,5 +27,5 @@ def get_posts():
 @app.get("/",include_in_schema=False)
 @app.get("/post",include_in_schema=False)
 def get_post(request: Request):
-    return templates.TemplateResponse(request,"home.html")
+    return templates.TemplateResponse(request,"home.html",{"posts": post})
 
